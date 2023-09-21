a582b0f4e9d2a32c00307d9b1990ac7dc440d7ab
mrantip <108371711+mrantip@users.noreply.github.com>
2023-09-21 22:14:09 +0300

    v2 ДЗ 2

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -3,18 +3,18 @@ from selene import be, have
 from selene.support.shared import browser
 
 
-@pytest.fixture
-def browser_window(scope='function'):
+@pytest.fixture(scope='function')
+def browser_window():
     browser.config.window_width = 1280
     browser.config.window_height = 720
-    yield
-    browser.close()
 
 
-def test_find_true():
+
+def test_find_true(browser_window):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
+
 
 def test_find_false():
     type_text = 'dfbdsjfbdjbdsbdshfdshfbdhsfbhdf'
